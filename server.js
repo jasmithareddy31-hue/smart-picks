c9ea9b8bc4f2a4ff8344eb18857fb2c56207b102
jasmithareddy31-hue <jasmithareddy31@gmail.com>
2026-03-24 18:06:20 +0530

    Refactor server to use dynamic port configuration
    Updated server to use environment variable for PORT.

--- server.js
+++ server.js
@@ -53,4 +53,5 @@ app.delete('/api/products/:id', async (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
